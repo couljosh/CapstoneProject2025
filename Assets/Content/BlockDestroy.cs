96f9c218daf306b95cb80ab483c42dc2e098e91a
couljosh <127234391+couljosh@users.noreply.github.com>
2025-09-12 18:31:41 -0400

    Pushed to save import errors

---
+++ Assets/Content/BlockDestroy.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class BlockDestroy : MonoBehaviour
+{
+
+    public float destroyDelay;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ObjectDestroyer")
+        {
+            disableCubeAfterDelay();
+        }
+    }
+
+    public void disableCubeAfterDelay()
+    {
+        StartCoroutine(disableCube());
+    }
+
+    public IEnumerator disableCube()
+    {
+        yield return new WaitForSeconds(0.07f);
+
+        //make mesh invisible
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+        //make box trigger only
+        gameObject.GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    public void enableCube()
+    {
+        //make mesh invisible
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+        //re-enable collision
+        gameObject.GetComponent<BoxCollider>().isTrigger = false;
+    }
+}
