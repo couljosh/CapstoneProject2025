3ae548ea2d9be24b94e71d74f2ddb83b96a5d907
couljosh <127234391+couljosh@users.noreply.github.com>
2025-11-24 12:50:18 -0500

    Added Lowered Ground Tilemap Framework

---
+++ Assets/Scripts/Round Management/LevelSelection.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    public static void LoadChosenLevel()
+    {
+
+    }
+
+}
+
