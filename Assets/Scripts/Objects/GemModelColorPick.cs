4ebe7519556902493be3a79a1def36b73196888c
ReversityVII <127150723+ReversityVII@users.noreply.github.com>
2025-12-04 19:24:09 -0500

    Added new gem models to all levels
    implemented new gem models. all levels have been updated.

---
+++ Assets/Scripts/Objects/GemModelColorPick.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GemModelColorPick : MonoBehaviour
+{
+    public GemRef gemRef;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //pick mesh, material and size
+        int rand = Random.Range(0, gemRef.meshes.Count);
+        gameObject.GetComponent<MeshFilter>().mesh = gemRef.meshes[rand];
+        gameObject.GetComponent<MeshRenderer>().material = gemRef.materials[rand];
+        gameObject.transform.localScale = (gemRef.scales[rand]);
+
+        //pick color
+        rand = Random.Range(0, gemRef.colors.Count);
+        gameObject.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", gemRef.colors[rand]);
+        gameObject.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", gemRef.emiColors[rand]);
+
+    }
+    
+}
