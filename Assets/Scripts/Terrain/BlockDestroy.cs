cd91adc10457f927d5c7430a853a549d6bc8a1a4
pgreek <117470140+pgreek@users.noreply.github.com>
2025-12-07 22:50:02 -0500

    Added Terrain Destroy VFX
    (Modified BlockDestroy and the terrain prefabs which ref a vfx prefab)
    
    For performance, the vfx is instantiated upon terrain destroyed then the vfx is destroyed after.

--- Assets/Scripts/Terrain/BlockDestroy.cs
+++ Assets/Scripts/Terrain/BlockDestroy.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Tilemaps;
-using NUnit.Framework.Constraints;
 
 public class BlockDestroy : MonoBehaviour
 {
@@ -14,93 +13,65 @@ public class BlockDestroy : MonoBehaviour
     public GameObject startingType;
     public GameObject emptyType;
 
-    private TileBase tilebase;
 
-
+    [Header("VFX")]
+    public GameObject destructionVFX; 
 
     private void Start()
     {
         terrainTileMap = GameObject.Find("TerrainTileMap").GetComponent<Tilemap>();
-
         myCellLocation = terrainTileMap.WorldToCell(transform.position);
-        //print(myCellLocation);
-
         startingType = this.gameObject;
     }
 
-    //Dectruction on Collsion
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "ObjectDestroyer")
         {
             disableCubeAfterDelay(destroyDelay);
-
-
         }
     }
 
     public void disableCubeAfterDelay(float delay)
     {
         StartCoroutine(disableCube(delay));
-        
     }
-
 
     public IEnumerator disableCube(float delay)
     {
-
         yield return new WaitForSeconds(delay);
 
+        if (destructionVFX != null)
+        {
+            GameObject vfx = Instantiate(destructionVFX, transform.position, Quaternion.identity);
+            //Destroy(vfx, 2f); //auto-destroy after finished
+        }
+
         gameObject.tag = "InactiveTerrain";
-
-
-        //make mesh invisible
         gameObject.GetComponent<MeshRenderer>().enabled = false;
-        //make box trigger only
         gameObject.GetComponent<BoxCollider>().isTrigger = true;
-        terrainTileMap.SetTile(myCellLocation, null);
 
+        terrainTileMap.SetTile(myCellLocation, null);
     }
 
     public void disableCubeNonCoroutine()
     {
+        if (destructionVFX != null)
+        {
+            GameObject vfx = Instantiate(destructionVFX, transform.position, Quaternion.identity);
+            //Destroy(vfx, 2f);
+        }
+
         gameObject.tag = "InactiveTerrain";
-
-
-        //make mesh invisible
         gameObject.GetComponent<MeshRenderer>().enabled = false;
-        //make box trigger only
         gameObject.GetComponent<BoxCollider>().isTrigger = true;
-
         terrainTileMap.SetTile(myCellLocation, null);
     }
-
 
     public void enableCube()
     {
         gameObject.tag = "ActiveTerrain";
-
-        //make mesh invisible
         gameObject.GetComponent<MeshRenderer>().enabled = true;
-        //re-enable collision
         gameObject.GetComponent<BoxCollider>().isTrigger = false;
-    }
-
-    public void RefreshTile(ref TileData t)
-    {
-
-        for (int x = -1; x < 1; x++)
-        {
-            for (int y = 1; y > -1; y--)
-            {
-
-            }
-        }
-
-    }
-
-    public void GetTileData()
-    {
-
     }
 }
