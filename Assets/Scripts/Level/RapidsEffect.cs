7a865ad6a02b8fae9dc01fa883d06603ae75884a
J-galit <60620874+J-galit@users.noreply.github.com>
2026-01-08 15:59:13 -0500

    Started Prototyping the Water Rapids

---
+++ Assets/Scripts/Level/RapidsEffect.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+public class RapidsEffect : MonoBehaviour
+{
+    public float rapidForce;
+    public float gemBob;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(transform.forward * rapidForce, ForceMode.VelocityChange);
+
+        if (other.gameObject.tag == "Gem")
+        {
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * gemBob, ForceMode.VelocityChange);
+        }
+        
+    }
+
+
+
+
+
+    
+}
