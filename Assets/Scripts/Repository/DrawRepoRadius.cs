e8130d955b8a0e3cf4b9975dfd8d2bfea4bff5fa
J-galit <60620874+J-galit@users.noreply.github.com>
2026-01-08 12:53:18 -0500

    Added a radius gizmo on the spawn points of the repo

---
+++ Assets/Scripts/Repository/DrawRepoRadius.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+
+public class DrawRepoRadius : MonoBehaviour
+{
+    //Code for showing the radius of the repository when it pops up
+    //Makes it easier to place repo markers
+
+    public float radius = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+       
+
+   private void OnDrawGizmos()
+     {
+            Handles.color = Color.yellow;
+            Handles.DrawWireDisc(transform.position, Vector3.up, radius);
+     }
+    
+
+}
