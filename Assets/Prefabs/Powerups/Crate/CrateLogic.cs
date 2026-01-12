6f7225ea91cd1597f675bc8ccf136238c99efae8
pgreek <petergeorgak2004@gmail.com>
2026-01-12 17:01:11 -0500

    Crate Tracked on Camera and Fixed Crate Size

--- Assets/Prefabs/Powerups/Crate/CrateLogic.cs
+++ Assets/Prefabs/Powerups/Crate/CrateLogic.cs
@@ -3,25 +3,45 @@ using UnityEngine;
 public class CrateLogic : MonoBehaviour
 {
     public GameObject[] powerUps;
-    public GameObject chosenPowerup; 
-    
+    public GameObject chosenPowerup;
+
+    private DynamicCameraFollow dynamicCamera;
+
+    void Awake()
+    {
+        dynamicCamera = Object.FindFirstObjectByType<DynamicCameraFollow>();
+    }
+
     void Start()
     {
         int randInd = Random.Range(0, powerUps.Length);
         chosenPowerup = powerUps[randInd];
+
+        if (dynamicCamera != null)
+        {
+            dynamicCamera.AddPlayer(transform);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Repository")
+        if (other.CompareTag("Repository"))
         {
             Destroy(gameObject);
         }
 
-        if (other.gameObject.tag == "ActiveTerrain")
+        if (other.CompareTag("ActiveTerrain"))
         {
-            other.gameObject.GetComponent<BlockDestroy>().disableCubeAfterDelay(0);
+            other.GetComponent<BlockDestroy>()
+                 ?.disableCubeAfterDelay(0);
         }
+    }
 
+    private void OnDestroy()
+    {
+        if (dynamicCamera != null)
+        {
+            dynamicCamera.RemovePlayer(transform);
+        }
     }
 }
