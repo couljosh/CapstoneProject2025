2111aa662d2de83476ce567c09290a593a7829fe
couljosh <127234391+couljosh@users.noreply.github.com>
2026-01-21 16:07:16 -0500

    Revamped EndScreen (gempile) WIP
    end screen shows gems falling into two piles:
    - can control how long it takes to spawn all gems
    - uses simple version of gems

--- Assets/GemSpawner.cs
+++ Assets/GemSpawner.cs
@@ -5,17 +5,17 @@ public class GemSpawner : MonoBehaviour
 {
     public int TeamNum;
 
-    public GameObject gemPrefab;
-    public GameObject gemSpawnloc;
+    public GameObject[] gemPrefab;
     public int gemAmount;
     public int gemCurrentCount;
 
-
+    public float totalSpawnTime = 5f;
     public float elapsedTime;
     public float spawnDelay;
 
     void Awake()
     {
+        //Assigns the corrent amount of gems to the corrisponding spawner
         if(TeamNum == 1)
         {
             gemAmount = GameScore.redTotalScore;
@@ -24,19 +24,32 @@ public class GemSpawner : MonoBehaviour
         {
             gemAmount = GameScore.blueTotalScore;
         }
+
+        //Ensures spawning completes at a specific time into the intermission screen
+        spawnDelay = totalSpawnTime / gemAmount;
     }
 
-
+     
     public void Update()
     {
         elapsedTime += Time.deltaTime;
 
         if(elapsedTime > spawnDelay && gemCurrentCount < gemAmount)
         {
-            gemCurrentCount ++; 
+            gemCurrentCount ++;
+
+            //Random position and rotation to ensure scattered drop of gems
+            var bounds = gameObject.GetComponent<BoxCollider>().bounds;
+            var px = Random.Range(bounds.min.x, bounds.max.x);
+            var py = Random.Range(bounds.min.y, bounds.max.y);
+            var pz = Random.Range(bounds.min.z, bounds.max.z);
+            Vector3 pos = new Vector3(px, py, pz);
             Quaternion randRot = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0,360));
-             GameObject.Instantiate(gemPrefab, transform.position, randRot);
-            elapsedTime = 0;
+
+            int chosenGem = Random.Range(0, gemPrefab.Length);
+            GameObject.Instantiate(gemPrefab[chosenGem], pos, randRot);
+                        
+             elapsedTime = 0;
         }
 
     }
--- Assets/Scripts/Objects/GemModelColorPick.cs
+++ Assets/Scripts/Objects/GemModelColorPick.cs
@@ -3,25 +3,35 @@ using UnityEngine;
 public class GemModelColorPick : MonoBehaviour
 {
     public GemRef gemRef;
+    public bool needsModel;
+    public bool needsColor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //pick mesh, material and size
         int rand = Random.Range(0, gemRef.meshes.Count);
-        gameObject.GetComponent<MeshFilter>().mesh = gemRef.meshes[rand];
-        gameObject.GetComponent<MeshRenderer>().material = gemRef.materials[rand];
-        gameObject.transform.localScale = (gemRef.scales[rand]);
 
-        //pick color
-        rand = Random.Range(0, gemRef.colors.Count);
-        gameObject.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", gemRef.colors[rand]);
-        gameObject.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", gemRef.emiColors[rand]);
+        if (needsModel)
+        {
+            //pick mesh, material and size
+            gameObject.GetComponent<MeshFilter>().mesh = gemRef.meshes[rand];
+            gameObject.GetComponent<MeshRenderer>().material = gemRef.materials[rand];
+            gameObject.transform.localScale = (gemRef.scales[rand]);
+
+        }
+
+        if (needsColor)
+        {
+            //pick color
+            rand = Random.Range(0, gemRef.colors.Count);
+            gameObject.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", gemRef.colors[rand]);
+            gameObject.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", gemRef.emiColors[rand]);
+        }
 
 
         //pick random rotation
-        gameObject.transform.rotation = Quaternion.Euler(Random.Range(0,360), Random.Range(0, 360), Random.Range(0, 360));
+        gameObject.transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
 
     }
-    
+
 }
