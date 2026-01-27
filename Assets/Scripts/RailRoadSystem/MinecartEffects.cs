8fac7cc7611b8dc4fce6f2e1962a784758690467
couljosh <127234391+couljosh@users.noreply.github.com>
2026-01-27 18:23:04 -0500

    Added Minecarft Red Signifiers

--- Assets/Scripts/RailRoadSystem/MinecartEffects.cs
+++ Assets/Scripts/RailRoadSystem/MinecartEffects.cs
@@ -7,6 +7,7 @@ public class MinecartEffects : MonoBehaviour
     public Material playerKillSpeedMaterial;
 
     private CartContact cartContact;
+    private MinecartMovement minecartMovement;
     private Rigidbody rb;
     public MeshRenderer modelMeshRenderer;
 
@@ -16,11 +17,22 @@ public class MinecartEffects : MonoBehaviour
 
     public bool isPowered;
 
+    public Light frontLight;
+    public Light backLight;
+    public Color lowSpeedLight;
+    public Color highSpeedLight;
+
+    public float lowSpeedIntensity;
+    public float highSpeedIntensity;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         defaultMaterial = modelMeshRenderer.material;
         cartContact = gameObject.GetComponentInChildren<CartContact>();
+        minecartMovement = gameObject.GetComponent<MinecartMovement>();
         rb = gameObject.GetComponent<Rigidbody>();
         
     }
@@ -28,16 +40,45 @@ public class MinecartEffects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (rb.linearVelocity.magnitude > cartContact.playerThreshold || isPowered)
         {
             modelMeshRenderer.material = playerKillSpeedMaterial;
             outline.OutlineColor = highSpeedOutlineColor;
 
+            if (minecartMovement.isForward)
+            {
+                //danger lights
+                frontLight.color = highSpeedLight;
+                frontLight.intensity = highSpeedIntensity;
+
+                //force opposite lights back to normal
+                backLight.color = lowSpeedLight;
+                backLight.intensity = lowSpeedIntensity;
+
+            }
+
+            if (!minecartMovement.isForward)
+            {
+                //danger lights
+                backLight.color = highSpeedLight;
+                backLight.intensity = highSpeedIntensity;
+
+                //force opposite lights back to normal
+                frontLight.color = lowSpeedLight;
+                frontLight.intensity = lowSpeedIntensity;
+            }
         }
         else
         {
             modelMeshRenderer.material = defaultMaterial;
             outline.OutlineColor = lowSpeedOutlineColor;
+
+            //reset light values
+            frontLight.color = lowSpeedLight;
+            backLight.color = lowSpeedLight;
+            frontLight.intensity = lowSpeedIntensity;
+            backLight.intensity = lowSpeedIntensity;
         }
     }
 }
